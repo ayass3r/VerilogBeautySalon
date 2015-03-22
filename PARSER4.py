98fc7bdd669e31a44ccfd62cfbb72bc07d7c0a2a
ahmed <ahmedwael@aucegypt.edu>
2015-03-23 00:05:57 +0200

    comments added

--- PARSER4.py
+++ PARSER4.py
@@ -1,27 +1,30 @@
+#this library must be imported in order to use regex expression
 import re
 
-
-
+#The Wire Class, all wires, inputs and outputs of the gatelevel netlist will be represented using this
+#the attributes and their default values are shown
 class Wire():
 	name = "defaultname"
 	isInput = False
 	isOutput = False
 	source = "defaultname"
-	ports = []
 	level = 0
-	
 
+#The Gate Class, all gates in the gatelevel netlist will be represented using this
+#the attributes and their default values are shown
 class Gate():
-	gtype = "defaulttype"
-	name = "defaultname"
-	inputport1 = "defaultname"
-	inputport2 = "defaultname"
-	outputport = "defaultname"
-	level = 0
-	connected1 = False
-	connected2 = False
+	gtype = "defaulttype" #the gate type, ex/ INV, AND, XOR, etc.
+	name = "defaultname"  #the unique identifier of the gate
+	inputport1 = "defaultname" #the name of the input/gate connected to the first input of the gate
+	inputport2 = "defaultname" #the name of the input/gate connected to the second input of the gate
+	outputport = "defaultname" #the name of the wire/output connected to the first input of the gate
+	level = 0 #the level of the gate
+	connected1 = False #boolean needed in the sorting 
+	connected2 = False #boolean needed in the sorting 
 
-def parseInv(gatelist, k, m, verilogfile, assignlist):
+#function called when a regex expression captures a line of a INVX1 gate, it will read the
+# following two lines, capturing data needed to update the gate attributes, and a list is returned to update other data structures
+def parseInv(gatelist, k, m, verilogfile, assignlist): 
 	gatelist.append(Gate())
 	gatelist[k].gtype = "INVX1"
 	gatelist[k].name = m.group(1)
@@ -46,6 +49,7 @@ def parseInv(gatelist, k, m, verilogfile, assignlist):
 		gatelist[k].outputport = s
 	returnlist.append(s)
 	return returnlist
+#same as parseInV, but used for all two input ports except the Flip Flop
 def parseRest(gatedescription, gatelist, k, m, verilogfile, assignlist):
 	gatelist.append(Gate())
 	gatelist[k].gtype = gatedescription
@@ -79,6 +83,7 @@ def parseRest(gatedescription, gatelist, k, m, verilogfile, assignlist):
 		gatelist[k].outputport = s
 	returnlist.append(s)
 	return returnlist
+#same as parseFF, but used to for the Flip Flop level
 def parseFF(gatelist, k, m, verilogfile, assignlist):
 	gatelist.append(Gate())
 	gatelist[k].gtype = "DFFPOSX1"
@@ -112,6 +117,9 @@ def parseFF(gatelist, k, m, verilogfile, assignlist):
 		gatelist[k].outputport = s
 	returnlist.append(s)
 	return returnlist
+
+#pseudo topological sorting, it iterates on all the gates and checks if it connected to an input  and connects it
+#it returns the number of gates found to be already connected, and a list (nextgatelist) of all gates found in level 2
 def getfirstlevel(gatelist, prevgatelist):
 	c = 0
 	nextgatelist = {}
@@ -133,6 +141,8 @@ def getfirstlevel(gatelist, prevgatelist):
 	returnlist = [c, nextgatelist]
 	return returnlist
 
+#pseudo topological sorting, it iterates on all the gates and checks if it is connected to gates in level n (prevgatelis) and connected to it and connects it
+#it returns the number of gates found to be already connected, and a list of all gates found in level n +1 (nextgatelist)
 def getnextlevel(gatelist, prevgatelist):
 	c = 0
 	nextgatelist = {}
@@ -157,6 +167,7 @@ def getnextlevel(gatelist, prevgatelist):
 	return returnlist
 
 def main():
+	#open the file specified by the filePath
 	filePath = "/home/ahmed/Desktop/booth.g.v"
 	verilogfile = open(filePath)
 	if (verilogfile.closed):
@@ -164,6 +175,7 @@ def main():
 	else:
 		print ("File Open")
 
+	#this loop will capture all assign statements 
 	assignlist = {}
 	for x in verilogfile:
 		p = re.compile("\s*assign\s*(\s*\S+\s*\S*)\s*=\s*(\s*\S+\s*\S*)\s*;") 
@@ -175,13 +187,14 @@ def main():
 	
 	verilogfile.close()
 
+	#open the file again in order to capture the wires, inputs, outputs, and gates
 	verilogfile = open(filePath)
 	if (verilogfile.closed):
 		print ("File failed to Open")
 	else:
 		print ("File Open")
 	
-
+	#nested if statements needed for capturing, the for loops reads the file line by line	
 	k =0
 	wiredict = {}
 	inputdict = {}
@@ -304,16 +317,19 @@ def main():
 																wiredict[sourcename[3]].source = sourcename[0]
 																k = k+1
 
+	#output all the wires, inputs, outputs
 	for j in wiredict:
 		print (wiredict[j].name+" "+str(wiredict[j].isInput)+" "+str(wiredict[j].isOutput)+" "+wiredict[j].source)
 
+	#returns the first level of gates, 
 	returnlist = getfirstlevel(gatelist, inputdict)
 	i = 0
+	#return subsequent levels of gates until all the gates are connected
 	while(len(gatelist) > returnlist[0]):
 		returnlist = getnextlevel(gatelist, returnlist[1])
 		i = i +1
 		
-	
+	#this format of the gatelist will have the ports of the gates naming the gates connected to it, not the wire names
 	copyGatelist = gatelist
 	for i, h in enumerate(copyGatelist):
 		if((wiredict[copyGatelist[i].inputport1].isInput == False) and (wiredict[copyGatelist[i].inputport1].isOutput == False)):
@@ -324,6 +340,7 @@ def main():
 		if((wiredict[copyGatelist[i].outputport].isInput == False) and (wiredict[copyGatelist[i].outputport].isOutput == False)):
 			copyGatelist[i].outputport = wiredict[copyGatelist[i].outputport].source
 
+	#Print all the gates with all their attributes
 	for h, z in enumerate(copyGatelist):
 		print(copyGatelist[h].name+" "+copyGatelist[h].gtype+" "+str(copyGatelist[h].level)+" "+str(copyGatelist[h].connected1)+" "+str(copyGatelist[h].connected2)+" "+copyGatelist[h].inputport1+" "+copyGatelist[h].inputport2+" "+copyGatelist[h].outputport)
 
